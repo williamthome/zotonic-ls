f9fe1358c67c3bd85f7c2181ecb627050405f223
williamthome <willegp88@gmail.com>
2022-06-28 20:52:43 -0300

    chore: use jest only for unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -141,7 +141,7 @@ export default {
     // testLocationInResults: false,
 
     // The glob patterns Jest uses to detect test files
-    testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+    testMatch: ['**/?(*.)+(spec).ts?(x)'],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
