401789948610194b693827ce0f8b427290bfc1ed
williamthome <willegp88@gmail.com>
2022-07-03 14:31:11 -0300

    feat(tests): transformSnippetSpy

--- src/__tests__/domain/__spies__/snippet-spy.ts
+++ src/__tests__/domain/__spies__/snippet-spy.ts
@@ -1,3 +1,4 @@
+import { TransformSnippet } from '@/domain/file';
 import { GetSnippets } from '@/domain/snippet';
 import { buildSpy } from '@/__tests__/__utils__';
 
@@ -5,4 +6,8 @@ export function getSnippetsSpy() {
     return buildSpy<GetSnippets>(() => {
         return Promise.resolve([]);
     });
+}
+
+export function transformSnippetSpy() {
+    return buildSpy<TransformSnippet>(({ snippet }) => snippet);
 }
