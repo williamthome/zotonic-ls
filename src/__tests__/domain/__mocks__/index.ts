5170aba2fddde749b2910bfb9468e892fc5b3a0b
williamthome <willegp88@gmail.com>
2022-07-05 05:28:42 -0300

    feat(tests): snippet mock

--- src/__tests__/domain/__mocks__/index.ts
+++ src/__tests__/domain/__mocks__/index.ts
@@ -1,1 +1,2 @@
 export * from './file-mock';
+export * from './snippet-mock';
--- src/__tests__/domain/__spies__/snippet-spy.ts
+++ src/__tests__/domain/__spies__/snippet-spy.ts
@@ -1,10 +1,11 @@
 import { TransformSnippet } from '@/domain/file';
 import { GetSnippets } from '@/domain/snippet';
 import { buildSpy } from '@/__tests__/__utils__';
+import { snippetMock } from '../__mocks__';
 
 export function getSnippetsSpy() {
     return buildSpy<GetSnippets>(() => {
-        return Promise.resolve([]);
+        return Promise.resolve([snippetMock()]);
     });
 }
 
