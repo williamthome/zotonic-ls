5768703e647e92ef6aee9f9ea832ad3d170ce408
williamthome <willegp88@gmail.com>
2022-07-05 06:58:34 -0300

    feat(tests): add expects

--- src/__tests__/__utils__/expect.ts
+++ src/__tests__/__utils__/expect.ts
@@ -8,10 +8,26 @@ export function expectNotEqual<A, B>(a: A, b: B) {
     return expect(a).not.toStrictEqual(b);
 }
 
+export function expectBeArray<T>(arr: T) {
+    return expect(arr).toStrictEqual(expect.any(Array));
+}
+
+export function expectNotEmpty<T extends Array<unknown>>(arr: T) {
+    return expect(arr.length).toBeGreaterThan(0);
+}
+
 export function expectThrowException<Fn extends AnyFunction | AnyPromise>(
     fn: Fn,
 ) {
     return fn instanceof Promise
         ? expect(fn).rejects.toThrow()
         : expect(fn).toThrow();
+}
+
+export function expectInstanceOf<V, I>(value: V, instance: I) {
+    return expect(value).toBeInstanceOf(instance);
+}
+
+export function expectAny<T>(value: T) {
+    return expect.any(value);
 }
