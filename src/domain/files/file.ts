4d5acfccc4ff7da17c1ccead35efa42e6092abf9
williamthome <willegp88@gmail.com>
2022-07-06 23:56:06 -0300

    refactor: change file to be immutable

--- src/domain/files/file.ts
+++ src/domain/files/file.ts
@@ -1,8 +1,10 @@
+import { immutable } from '../../common/functional-programming';
+
 export function buildFile(args: { name: string; path: string }) {
-    return {
+    return immutable({
         name: args.name,
         path: args.path,
-    };
+    });
 }
 
 export type File = ReturnType<typeof buildFile>;
