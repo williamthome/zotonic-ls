bc830b6e51df16798a0b5cab55bcaec56076d618
williamthome <willegp88@gmail.com>
2022-07-14 20:30:50 -0300

    refactor: remove comments

--- src/__tests__/domain/__spies__/file-spy.ts
+++ src/__tests__/domain/__spies__/file-spy.ts
@@ -1,18 +1,6 @@
-// TODO: Check unused functions
-
-import {
-    FilenameRegexByWorkspace,
-    FilesByGlobPattern,
-    // FilesByWorkspaces,
-} from '@/domain/files';
+import { FilenameRegexByWorkspace, FilesByGlobPattern } from '@/domain/files';
 import { buildSpy } from '@/__tests__/__utils__';
 import { fileMock } from '@/__tests__/domain/__mocks__/';
-
-// export function filesByWorkspaceSpy() {
-//     return buildSpy<FilesByWorkspaces>(() => {
-//         return Promise.resolve([fileMock()]);
-//     });
-// }
 
 export function filesByGlobPatternSpy() {
     return buildSpy<FilesByGlobPattern>(() => {
